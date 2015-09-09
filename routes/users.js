1293441e309217345479cc1abeb2aa6b60dc9073
John Wu <webmaster@leapoahead.com>
2015-09-09 16:50:51 -0700

    Use bluebird as promise library

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var Datastore = require('nedb');
+var path = require('path');
+
+var db = new Datastore({
+  autoload: true,
+  filename: path.resolve(__dirname, '../datastore.txt')
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
