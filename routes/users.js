463ab6e931b0249f5c77be7a4d3254e66870b2b8
John Wu <webmaster@leapoahead.com>
2015-09-09 17:43:58 -0700

    Merge projects into response

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,17 @@ var User = require('../lib/User');
 router.get('/:id', function(req, res, next) {
   var userId = parseInt(req.params.id, 10);
   User.getUserById(userId).then(function (user) {
-    res.json(user);
+    req.user = user;
+    next();
   });
+}, function (req, res, next) {
+  User.getUserProjects(req.user).then(function (projects) {
+    req.projects = projects;
+    next();
+  });
+}, function (req, res, next) {
+  req.user.projects = req.projects;
+  res.json(req.user);
 });
 
 module.exports = router;
