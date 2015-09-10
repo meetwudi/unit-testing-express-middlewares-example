8544758bf0d415c3250fa39cb89067c3a849d02e
John Wu <webmaster@leapoahead.com>
2015-09-09 17:19:28 -0700

    Add endpoint for getting user document

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var Datastore = require('nedb');
-var path = require('path');
+var User = require('../lib/User');
 
-var db = new Datastore({
-  autoload: true,
-  filename: path.resolve(__dirname, '../datastore.txt')
-});
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res, next) {
+  var userId = parseInt(req.params.id, 10);
+  User.getUserById(userId).then(function (user) {
+    res.json(user);
+  });
 });
 
 module.exports = router;
