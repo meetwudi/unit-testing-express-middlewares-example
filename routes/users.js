dcee0c52087218877ed2c941ffeff22d7c6f5e3b
John Wu <webmaster@leapoahead.com>
2015-09-09 18:24:15 -0700

    Separate middleware functions

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,35 @@ var express = require('express');
 var router = express.Router();
 var User = require('../lib/User');
 
-router.get('/:id', function(req, res, next) {
-  var userId = parseInt(req.params.id, 10);
-  User.getUserById(userId).then(function (user) {
-    req.user = user;
-    next();
-  });
-}, function (req, res, next) {
-  User.getUserProjects(req.user).then(function (projects) {
-    req.projects = projects;
-    next();
-  });
-}, function (req, res, next) {
-  req.user.projects = req.projects;
-  res.json(req.user);
-});
+var middlewares = {
+  getUserById: function (req, res, next) {
+    var userId = parseInt(req.params.id, 10);
+    User.getUserById(userId).then(function (user) {
+      req.user = user;
+      next();
+    });
+  },
 
-module.exports = router;
+  getProjectsForUser: function (req, res, next) {
+    User.getUserProjects(req.user).then(function (projects) {
+      req.projects = projects;
+      next();
+    });
+  },
+
+  responseUserWithProjects: function (req, res, next) {
+    req.user.projects = req.projects;
+    res.json(req.user);
+  }
+}
+
+router.get('/:id',
+  middlewares.getUserById,
+  middlewares.getProjectsForUser,
+  middlewares.responseUserWithProjects
+);
+
+module.exports = {
+  router: router,
+  middlewares: middlewares
+}
